0619d76d3940341b8cae5db26d63192a89c4756d
timhams <timtam54@hotmail.com>
2024-07-26 23:25:09 +1000

    AddSampleOnSiteReport

---
+++ Endpoints/SampleOnOffSiteActualEP.cs
@@ -0,0 +1,20 @@
+﻿using AllungaWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+namespace AllungaWebAPI.Endpoints;
+public static class SampleOnOffSiteActualEP
+{
+    public static void MapSampleOnOffSiteActualEPEndpoints(this IEndpointRouteBuilder routes)
+    {
+
+        var group = routes.MapGroup("/api/SampleOnOffSiteActual").WithTags(nameof(SampleOnOffSiteActualEP));
+
+        group.MapGet("/id/From/To", async (int id,DateTime From, DateTime To, dbcontext db) =>
+        {
+            return await db.schedule.FromSql($"exec SampleOnOffSiteActual {id},{From},{To}").ToListAsync();
+        })
+        .WithName("GetSampleOnOffSiteActual")
+        .WithOpenApi();
+    }
+}
+
+
